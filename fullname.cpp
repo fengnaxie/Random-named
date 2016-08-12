2c17eb06dc7e7f7a5d9caaa2209c124e234d8b8a
筱枫 <luotaiping@users.noreply.github.com>
2016-08-12 16:24:26 +0800

    增加错误处理函数
    提高程序健壮性，增加拷贝构造函数

--- fullname.cpp
+++ fullname.cpp
@@ -12,36 +12,84 @@
 #include <stdlib.h>
 #include <time.h>
 #include <memory.h>
+#include <stdarg.h>
 
 
 CStyle::CStyle(int _name_num)
 {
 	name_num = _name_num;
 
-	if (name_num <= 0)
-		return;
+    // 默认将所有的错误输出到屏幕上
+	open_error_file(NULL, stderr);
 
-	list = (struct Fullname *)malloc(sizeof(struct Fullname)*name_num);
+	if (name_num <= 0)
+    {
+        write_error("name_num 不能小于等于 0!\n");
+        return;
+    }
+
+	list = new struct Fullname[sizeof(struct Fullname)*name_num];
 
 	if (NULL == list)
-		return;
+    {
+        write_error("list表申请内存失败！\n");
+        return;
+    }
 
 	memset(list, 0, sizeof(struct Fullname)*name_num);
 
 	srand(time(NULL));
 
-    for (int i=0; i<4; i++)
-        store_buffer[0] = NULL;
+	for (int i=0; i<4; i++)
+       store_buffer[i] = NULL;
 }
 
-int CStyle::fullname(struct Fullname *fn, NAME_FLAGS flags)
-{
-	if (name_num <= 0)
-		return -2;
 
+CStyle::CStyle(const CStyle &cs)
+{
+    // 拷贝构造函数
+    name_num = cs.name_num;
+
+    list = new struct Fullname[sizeof(struct Fullname)*name_num];
+
+    if (NULL == list)
+    {
+        write_error("list表申请内存失败！\n");
+        return;
+    }
+
+    for (int i=0; i<name_num; i++)
+    {
+        list[i] = cs.list[i];
+    }
+
+    list = cs.list;
+    name = cs.name;
+
+    for (int i=0; i<4; i++)
+       store_buffer[i] = NULL;
+
+    if (strcmp(buffer_surname_female, cs.buffer_surname_female))
+        set_buffer(cs.buffer_surname_female, _single_surname);
+    else if(strcmp(buffer_surname_female2, cs.buffer_surname_female2))
+        set_buffer(cs.buffer_surname_female2, _double_surname);
+    else if(strcmp(buffer_name_female, cs.buffer_name_female))
+        set_buffer(cs.buffer_name_female, _single_name);
+    else if(strcmp(buffer_name_female2, cs.buffer_name_female2))
+        set_buffer(cs.buffer_name_female2, _double_name);
+}
+
+int CStyle::create(struct Fullname *fn, NAME_FLAGS flags)
+{
     // 随机生成
     if (_0 == flags)
         flags = NAME_FLAGS(rand()%6);
+
+    if (NULL == fn)
+    {
+        write_error("生成姓名失败！姓名缓冲区不能为空！\n");
+        return -1;
+    }
 
 	switch (flags)
 	{
@@ -78,51 +126,43 @@ int CStyle::fullname(struct Fullname *fn, NAME_FLAGS flags)
 
 int CStyle::_fullname(struct Fullname *fn)
 {
-	if (NULL == fn)
-		return -1;
-
 	// 重定向
 	surname = (struct Word *)buffer_surname_female;
 	name = (struct Word *)buffer_name_female;
 
-	fn->w[0] = surname[rand()%((strlen((char *)surname)-1)/2)];
-	fn->w[1] = name[rand()%((strlen((char *)name)-1)/2)];
+	fn->w[0] = surname[rand()%((strlen((char *)surname)-1)/2+1)];
+	fn->w[1] = name[rand()%((strlen((char *)name)-1)/2+1)];
 
 	return 0;
 }
 
 int CStyle::_fullname2(struct Fullname *fn)
 {
-	if (NULL == fn)
-		return -1;
-
 	// 重定向
 	surname = (struct Word *)buffer_surname_female;
 	name = (struct Word *)buffer_name_female;
 
-	fn->w[0] = surname[rand()%((strlen((char *)surname)-1)/2)];
-	fn->w[1] = name[rand()%((strlen((char *)name)-1)/2)];
-	fn->w[2] = name[rand()%((strlen((char *)name)-1)/2)];
+	fn->w[0] = surname[rand()%((strlen((char *)surname)-1)/2+1)];
+	fn->w[1] = name[rand()%((strlen((char *)name)-1)/2+1)];
+	fn->w[2] = name[rand()%((strlen((char *)name)-1)/2+1)];
 
 	return 0;
 }
 
 int CStyle::_fullname22(struct Fullname *fn)
 {
-	if (NULL == fn)
-		return -1;
-
 	int i;
 
 	// 重定向
 	surname = (struct Word *)buffer_surname_female;
 	name = (struct Word *)buffer_name_female2;
 
-	i = rand()%((strlen((char *)name)-1)/2);
+	i = rand()%((strlen((char *)name)-1)/2+1);
 
+    // 确保i为2的倍数
 	if (i%2 && i!=0) i--;
 
-	fn->w[0] = surname[rand()%((strlen((char *)surname)-1)/2)];
+	fn->w[0] = surname[rand()%((strlen((char *)surname)-1)/2+1)];
 	fn->w[1] = name[i];
 	fn->w[2] = name[i+1];
 
@@ -131,63 +171,57 @@ int CStyle::_fullname22(struct Fullname *fn)
 
 int CStyle::_full2name(struct Fullname *fn)
 {
-	if (NULL == fn)
-		return -1;
-
 	int i;
 
 	// 重定向
 	surname = (struct Word *)buffer_surname_female2;
 	name = (struct Word *)buffer_name_female;
 
-	i = rand()%((strlen((char*)surname)-1)/2);
+	i = rand()%((strlen((char*)surname)-1)/2+1);
 
+    // 确保i为2的倍数
 	if (i%2 && i!= 0) i--;
 
 	fn->w[0] = surname[i];
 	fn->w[1] = surname[i+1];
-	fn->w[2] = name[rand()%((strlen((char *)name)-1)/2)];
+	fn->w[2] = name[rand()%((strlen((char *)name)-1)/2+1)];
 
 	return 0;
 }
 
 int CStyle::_full2name2(struct Fullname *fn)
 {
-	if (NULL == fn)
-		return -1;
-
 	int i;
 
 	// 重定向
 	surname = (struct Word *)buffer_surname_female2;
 	name = (struct Word *)buffer_name_female;
 
-	i = rand()%((strlen((char*)surname)-1)/2);
+	i = rand()%((strlen((char*)surname)-1)/2+1);
 
+    // 确保i为2的倍数
 	if (i%2 && i!= 0) i--;
 
 	fn->w[0] = surname[i];
 	fn->w[1] = surname[i+1];
-	fn->w[2] = name[rand()%((strlen((char *)name)-1)/2)];
-	fn->w[3] = name[rand()%((strlen((char *)name)-1)/2)];
+	fn->w[2] = name[rand()%((strlen((char *)name)-1)/2+1)];
+	fn->w[3] = name[rand()%((strlen((char *)name)-1)/2+1)];
 
 	return 0;
 }
 
 int CStyle::_full2name22(struct Fullname *fn)
 {
-	if (NULL == fn)
-		return -1;
-
 	int i, j;
 
 	// 重定向
 	surname = (struct Word *)buffer_surname_female2;
 	name = (struct Word *)buffer_name_female2;
 
-	i = rand()%((strlen((char*)surname)-1)/2);
-	j = rand()%((strlen((char*)name)-1)/2);
+	i = rand()%((strlen((char*)surname)-1)/2+1);
+	j = rand()%((strlen((char*)name)-1)/2+1);
 
+    // 确保i与j为2的倍数
 	if (i%2 && i!=0) i--;
 	if (j%2 && j!=0) j--;
 
@@ -199,44 +233,64 @@ int CStyle::_full2name22(struct Fullname *fn)
 	return 0;
 }
 
-int CStyle::fullname_print(const struct Fullname *print)
+int CStyle::print(const struct Fullname *print)
 {
     if (NULL == print)
+    {
+        write_error("需打印的姓名不存在！\n");
         return -1;
+    }
 
-    printf("%c%c%", print->w[0].w[0], print->w[0].w[1]);
-    printf("%c%c%", print->w[1].w[0], print->w[1].w[1]);
-    printf("%c%c%", print->w[2].w[0], print->w[2].w[1]);
-    printf("%c%c%", print->w[3].w[0], print->w[3].w[1]);
+    printf("%c%c", print->w[0].w[0], print->w[0].w[1]);
+    printf("%c%c", print->w[1].w[0], print->w[1].w[1]);
+    printf("%c%c", print->w[2].w[0], print->w[2].w[1]);
+    printf("%c%c", print->w[3].w[0], print->w[3].w[1]);
     printf("\n");
     return 0;
 }
 
-int CStyle::setname(char *src, SETNAME_FLAGS flags)
+int CStyle::set_buffer(const char *src, SETNAME_FLAGS flags)
 {
+    static char *buffer = NULL;
+
 	if (NULL == src)
-		return -1;
+    {
+        write_error("待设置的缓冲区不存在！\n");
+    }
+
+    // 这段代码防止取余0错误
+    if ((strlen(src)-1)/2 == 0)
+    {
+        buffer = new char[strlen(src)*2];
+        strcpy(buffer, src);
+        strcat(buffer, src);
+    }
+    else
+    {
+        buffer = new char[strlen(src)];
+        strcpy(buffer, src);
+    }
 
 	switch (flags)
 	{
     case _single_surname:
         store_buffer[0] = buffer_surname_female;
-        buffer_surname_female = src;
+        buffer_surname_female = buffer;
         break;
 
     case _double_surname:
         store_buffer[1] = buffer_surname_female2;
-        buffer_surname_female2 = src;
+        buffer_surname_female2 = buffer;
         break;
 
     case _single_name:
         store_buffer[2] = buffer_name_female;
-        buffer_name_female = src;
+        buffer_name_female = buffer;
         break;
 
     case _double_name:
         store_buffer[3] = buffer_name_female2;
-        buffer_name_female2 = src;
+        buffer_name_female2 = buffer;
         break;
 
     default:
@@ -245,7 +299,7 @@ int CStyle::setname(char *src, SETNAME_FLAGS flags)
 	return 0;
 }
 
-int CStyle::restname(SETNAME_FLAGS flags)
+int CStyle::rest_buffer(SETNAME_FLAGS flags)
 {
     switch (flags)
     {
@@ -253,7 +307,7 @@ int CStyle::restname(SETNAME_FLAGS flags)
         if (NULL == store_buffer[0])
             return -1;
 
-        free(buffer_surname_female);
+        delete [] buffer_surname_female;
         buffer_surname_female = store_buffer[0];
         break;
 
@@ -261,7 +315,7 @@ int CStyle::restname(SETNAME_FLAGS flags)
         if (NULL == store_buffer[1])
             return -1;
 
-        free(buffer_surname_female2);
+        delete [] buffer_surname_female2;
         buffer_surname_female2 = store_buffer[1];
         break;
 
@@ -270,7 +324,7 @@ int CStyle::restname(SETNAME_FLAGS flags)
         if (NULL == store_buffer[2])
             return -1;
 
-        free(buffer_name_female);
+        delete [] buffer_name_female;
         buffer_name_female = store_buffer[2];
         break;
 
@@ -278,7 +332,7 @@ int CStyle::restname(SETNAME_FLAGS flags)
         if (NULL == store_buffer[3])
             return -1;
 
-        free(buffer_name_female2);
+        delete [] buffer_name_female2;
         buffer_name_female2 = store_buffer[3];
         break;
 
@@ -288,8 +342,74 @@ int CStyle::restname(SETNAME_FLAGS flags)
     return 0;
 }
 
+int CStyle::open_error_file(const char *filename, FILE *fp)
+{
+    if (error_fp)
+        close_error_file();
+
+    if (fp)
+    {
+        error_fp = fp;
+    }
+    else
+    {
+        error_fp = fopen(filename, "w");
+
+        if (NULL == error_fp)
+        {
+            fprintf(stderr, "打开错误记录文件：%s 失败！\n", filename);
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+int CStyle::write_error(const char *str, ...)
+{
+    if (NULL == error_fp)
+    {
+        fprintf(stderr, "错误文件指针不存在，请先打开错误文件！\n");
+        return -1;
+    }
+
+    char buffer[1024];
+    va_list args;
+
+    // 初始化可变参数并使用vsprintf输出到buffer中
+    va_start(args, str);
+    vsprintf(buffer, str, args);
+    va_end(args);
+
+    fprintf(error_fp, buffer);
+
+    // 刷新输出流
+    fflush(error_fp);
+
+    return 0;
+}
+
+int CStyle::close_error_file(void)
+{
+    if (error_fp)
+    {
+        fclose(error_fp);
+        error_fp = NULL;
+    }
+
+    return 0;
+}
+
+CStyle CStyle::clone(void)
+{
+    // 该函数复制自身并返回新的对象
+    return CStyle(*this);
+}
+
 CStyle::~CStyle()
 {
     if (name_num > 0)
-        free(list);
+        delete [] list;
+
+    close_error_file();
 }
--- test.cpp
+++ test.cpp
@@ -1,21 +1,17 @@
+#include <iostream>
 #include "fullname.h"
+#include "fullname.cpp"
 
 int main(int argc, char **argv)
 {
     CStyle cs(10);
-    cs.fullname(&cs.list[0], _0);
-    cs.fullname_print(&cs.list[0]);
 
-    cs.setname("ÄãºÃ");
-    cs.fullname(&cs.list[1]);
-    cs.fullname_print(&cs.list[1]);
-    cs.restname(_single_surname);
-
-    for (int i=2; i<10; i++)
-    {
-        cs.fullname(&cs.list[i], _0);
-        cs.fullname_print(&cs.list[i]);
-    }
-
+    cs.set_buffer("ä¸­å›½");
+    cs.open_error_file("error.txt");
+    cs.write_error("123%d", 1000);
+    cs.create(&cs.list[1]);
+    cs.print(&cs.list[1]);
+    cs.rest_buffer(_single_surname);
     return 0;
 }
+
